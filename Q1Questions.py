1622929daf0923792d0d459a3d748781afe6aef4
dankhatana <124597966+dankhatana@users.noreply.github.com>
2023-05-23 14:06:55 +0000

    added code

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,20 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+def save_questions(filename):
+    with open(filename, 'w') as file:
+        for _ in range(6):
+            question = input("Enter the question: ")
+            answers = []
+            for i in range(4):
+                answer = input(f"Enter answer {chr(65 + i)}: ")
+                answers.append(answer)
+            correct_answer = input("Enter the letter of the correct answer: ")
+            file.write(f"{question}\n")
+            for i in range(4):
+                file.write(f"{chr(65 + i)}. {answers[i]}\n")
+            file.write(f"{correct_answer}\n")
+            file.write('\n')
+        print("Questions saved successfully!")
+
+
+save_questions("questions.txt")
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,33 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+def load_questions(filename):
+    questions = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        for i in range(0, len(lines), 7):
+            question = lines[i].strip()
+            answers = [lines[i + j].strip() for j in range(1, 5)]
+            correct_answer = lines[i + 5].strip()
+            questions.append((question, answers, correct_answer))
+    return questions
+
+
+def pose_questions(questions):
+    score = 0
+    for i, (question, answers, correct_answer) in enumerate(questions, 1):
+        print(f"Question {i}:")
+        print(question)
+        for j, answer in enumerate(answers):
+            print(f"{chr(65 + j)}. {answer}")
+        user_answer = input("Your answer (enter the letter): ").upper()
+        if user_answer == correct_answer:
+            print("Correct!")
+            score += 1
+        else:
+            print(f"Incorrect. The correct answer is {correct_answer}.")
+        print()
+    print(f"You scored {score}/{len(questions)}.")
+
+
+questions = load_questions("questions.txt")
+pose_questions(questions)
